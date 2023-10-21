3c3363eef3f201678cff24907e91e99cc9b39531
tenzin palgyal <tenzinpalgyal@tenzins-MacBook-Air.local>
2023-10-21 13:11:46 +0530

    First Push

---
+++ app/page.tsx
@@ -0,0 +1,17 @@
+import Camp from "@/Components/Camp";
+import Features from "@/Components/Features";
+import GetApp from "@/Components/GetApp";
+import Guid from "@/Components/Guid";
+import Hero from "@/Components/Hero";
+
+export default function Home() {
+  return (
+    <>
+    <Hero/>
+    <Camp/>
+    <Guid/>
+    <Features/>
+    <GetApp/>
+    </>
+  )
+}
